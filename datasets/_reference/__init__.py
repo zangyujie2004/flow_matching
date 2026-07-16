feb59363d9e9d17dc3758b3fea525cb9d2ecdd72
zangyujie2004 <zangyujie@sjtu.edu.cn>
2026-07-16 12:08:09 +0800

    clear-up

--- datasets/_reference/__init__.py
+++
@@ -1,4 +0,0 @@
-from .robot_layout import robot_dim, robot_slice
-from .zarr_dataset import DataConfig, ZarrDataset
-
-__all__ = ["DataConfig", "ZarrDataset", "robot_dim", "robot_slice"]
--- models/_reference/policy/__init__.py
+++
@@ -1,4 +0,0 @@
-from .condition_encoder import ConditionEncoder, DinoV2SmallEncoder
-from .flow_policy import FlowMatchingPolicy
-
-__all__ = ["ConditionEncoder", "DinoV2SmallEncoder", "FlowMatchingPolicy"]
